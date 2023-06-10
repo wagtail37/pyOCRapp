4b3f22d7a2ee8d4c8e29874edd6a15d0d4cd6023
wagtail37 <sumida.k.ad@m.titech.ac.jp>
2023-06-10 21:58:26 +0900

    fix

--- pyocrSample.py
+++ pyocrSample.py
@@ -1,21 +1,3 @@
-# from PIL import Image
-# import pyocr
-# import os
-# import pyocr.builders
-
-# # pyocr.get_available_tools()のエラーの解消
-# pyocr.tesseract.TESSERACT_CMD = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
-# path = 'C:\Program Files\Tesseract-OCR'
-# os.environ['PATH'] = os.environ['PATH'] + path
-# tools = pyocr.get_available_tools()
-# tool = tools[0]
-# img = Image.open("Receipt2.png")
-# builder = pyocr.builders.TextBuilder(tesseract_layout=6)
-# text = tool.image_to_string(img, lang="jpn", builder=builder)
-# text = text.replace(' ', '')
-# print(text)
-
 from PIL import Image
 import pyocr
 
