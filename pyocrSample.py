fcf52aa7254afc53182a27d1b2c48c3489be2cb0
wagtail37 <sumida.k.ad@m.titech.ac.jp>
2023-06-10 22:04:52 +0900

    add comment

--- pyocrSample.py
+++ pyocrSample.py
@@ -7,5 +7,6 @@ engine = engines[0]
 
 # 画像の文字を読み込む
 builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+#日本語の場合はlang="jpn"を指定
 txt = engine.image_to_string(Image.open('imageSample.png'), lang="jpn",builder=builder)
 print(txt) 
